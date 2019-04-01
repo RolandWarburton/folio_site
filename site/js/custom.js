7ee43f9c003f3f932fd5740fd4a11d3451cacd15
Roland <warburtonroland@gmail.com>
2019-04-01 14:51:08 +1100

    cleaned up comments

--- site/js/custom.js
+++ site/js/custom.js
@@ -26,11 +26,11 @@ function shuffle(array) {
 return array;
 }
 
-// var $all = $("li")
-// $(shuffle($all).slice(0, $all.length-4)).toggleClass("glitch");
-// setTimeout(function(){$("li").removeClass("glitch");}, 1000);
-// console.log($all.length)
+$running = false
+$i = 0
+$counter = 0
 
+// glitch effect
 function glitch($all, remove, reset, slice_start, slice_end) {
       $running = true
       // pick random elements to glitch
@@ -48,32 +48,6 @@ function glitch($all, remove, reset, slice_start, slice_end) {
       }, timer1);
 }
 
-// function a() {
-//     var deferred = new $.Deferred();
-//     $all = $("li")
-//     glitch($all, 1000, 2000)
-//     console.log("test")
-//     // stuff -- resolve deferred once async method is complete
-//     setTimeout(function () {
-//         deferred.resolve();
-//     }, 2000)
-//
-//     return deferred.promise();
-// }
-//
-// function b() {
-//     console.log('b')
-//     $all = $("li")
-//     glitch($all, 1000, 2000)
-// }
-//
-//
-// a().then(b);
-
-$running = false
-$i = 0
-$counter = 0
-
 function glitch_init() {
 
 var myFunction = function() {
@@ -81,36 +55,45 @@ var myFunction = function() {
     // select all lists
     var $all = $(".title ul li,h5")
 
+    // case 0 = glitch
+    // case 1 = skip
+    // case 2 = skip
     $chance = Math.round(Math.random() * (2))
 
-    switch($chance) {
-      case 0:
-            if ($i == 0 && !$running) {
-                  glitch($all, 350, 5000, 0, 1)
-                  console.log("short glitch")
-                  $i = 0
-            }
-            $counter+=Math.round(Math.random() * (5))
-      break;
-      default:
-            $counter+=Math.round(Math.random() * (3))
-            if ($counter > 20 && !$running) {
-                  glitch($all, 250, 5000, 0, 2)
-                  console.log("long glitch")
-                  $counter = 0
-      } else {
-            console.log("skip")
-      }
-      break;
-    }
+    switch($chance)
+    {
+          case 0:
+          if ($i == 0 && !$running)
+          {
+                glitch($all, 350, 5000, 0, 1)
+                console.log("short glitch")
+                $i = 0
+          }
+          $counter+=Math.round(Math.random() * (5))
+          break;
 
-     // repeat for a set duration
+          // default case hits 2/3 of the time
+          default:
+          $counter+=Math.round(Math.random() * (3))
+          if ($counter > 20 && !$running)
+          {
+                glitch($all, 250, 5000, 0, 2)
+                console.log("long glitch")
+                $counter = 0
+          }
+          else
+          {
+                console.log("skip")
+          }
+          break;
+    }
+     // repeat function after 1s
      setTimeout(myFunction, 1000);
 }
 myFunction();
 }
 
-
+// init glitch effect loop
 glitch_init();
 
 
@@ -119,76 +102,14 @@ glitch_init();
 
 
 
-  // jQuery(this).prev("body").attr("id","title-content");
-//
-// setTimeout(function(){  });
-//
-// var counter = 0;
-// (function addGlitch() {
-//
-//   setTimeout(function() {
-//     if (counter<=10) {
-//          var r = Math.floor((Math.random() * 4)+1);
-//           var a = $( ".content ul:nth-child("+r+")")
-//
-//
-//           console.log(r);
-//
-//         $(a).attr("class","glitchy");
-//
-//       addGlitch();
-//     }
-// }, 1000);
-//
-// })();
-
-//
-// setTimeout(function(){  });
-//
-// var counter = 0;
-// (function addGlitch() {
-//
-//   setTimeout(function() {
-//     if (counter<=10) {
-//          var r = Math.floor((Math.random() * 4)+1);
-//           var a = $( ".content ul:nth-child("+r+")")
-//
-//
-//           console.log(r);
-//
-//          //  $("a").addClass("glitchy").delay(1000).queue(function(){
-//          //     $(this).removeClass("glitchy").dequeue();
-//          //     console.log("test");
-//          // });
-//
-//
-//
-//       addGlitch();
-//     }
-// }, 1000);
-//
-// })();
-//
-
-
-
-// delayedAlert();
-//
-// function delayedAlert() {
-// timeoutID = window.setTimeout(2000);
-// $('.title').addClass("glitchy")
-//                        .delay(500)
-//                        .queue(function() {
-//                            $(this).removeClass("glitchy");
-//                            $(this).dequeue();
-//                        });
-// }
-//
 
 
 
 
 
-// console.log("loaded");
+
+
+
+
 
 }); //CLOSE
