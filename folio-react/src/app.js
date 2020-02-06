c8632c68897b099940d02d3b0d25b4fe065b8801
RolandWarburton <warburtonroland@gmail.com>
2020-02-06 21:28:45 +1100

    :sparkles: Added a working page fade transition

--- folio-react/src/app.js
+++ folio-react/src/app.js
@@ -1,7 +1,6 @@
 import React, { Component, useState, useContext, useTransition, useEffect, Fragment } from 'react';
-import { Transition, CSSTransition } from 'react-transition-group';
-// import { TransitionGroup, CSSTransition } from "react-transition-group";
-import { BrowserRouter, Switch, Route, withRouter } from 'react-router-dom';
+import { TransitionGroup, CSSTransition, Transition, SwitchTransition } from 'react-transition-group'
+import { Switch, Route } from 'react-router-dom';
 import '../styles/styles.scss'
 
 import Title from './components/Title.js';
@@ -9,35 +8,29 @@ import Index from './components/Index.js';
 import Articles from './components/Articles';
 import Projects from './components/Projects';
 
-const FadeIn = ({ in: inProp }) => (
-	<CSSTransition in={inProp} timeout={500} appear unmountOnExit classNames='roll'>
-		{state => (
-			<div className="RightCol FlexCenter">
-				I am {state}
-			</div>
-		)}
-	</CSSTransition>
-);
+const routes = [
+	{ path: '/', name: 'Home', name: "Home", Component: Index },
+	{ path: '/Articles', name: 'Articles', Component: Articles },
+	{ path: '/Projects', name: 'Projects', Component: Projects },
+]
 
-const RouteMenu = ({ }) => (
+const RouteMenu = ({ location }) => (
 	<Switch location={location}>
-		<Route exact path="/" component={Index} />
-		<Route exact path="/Articles" component={Articles} />
-		<Route exact path="/Projects" component={Projects} />
-		{/* <Route exact path="/Misc" component={Index} /> */}
-		{/* <Route exact path="/Testing" component={Testing} /> */}
-		{/* <Route render={() => <div className="content">Page Not Found</div>} /> */}
+		{routes.map(route => (
+			<Route key={route.name} exact path={route.path} name={route.name} component={route.Component} />
+		))}
 	</Switch>
 );
+
 
 function App({ location }) {
 	const [prevPath, setPrevPath] = useState(null);
 	const [path, setPath] = useState(location.pathname)
-	
+	const timeout = { enter: 200, exit: 100 }
+
 	useEffect(() => {
 		const thisPath = location.pathname
-		const prevPath = path
-		if(thisPath != path) {
+		if (thisPath != path) {
 			setPath(location.pathname)
 			setPrevPath(path)
 		}
@@ -46,57 +39,17 @@ function App({ location }) {
 	return (
 		<Fragment>
 			<Title />
-			{/* <button onClick={() => { setEntered(!entered); }}>Toggle Entered</button> */}
-			{/* <button onClick={() => { console.log(path); }}>Toggle Entered</button> */}
 			<div className="RightCol FlexCenter">
-				<RouteMenu />
+				<TransitionGroup>
+					<SwitchTransition mode="out-in">
+						<CSSTransition timeout={timeout} classNames="fade" key={location.key}>
+							<RouteMenu location={location} />
+						</CSSTransition>
+					</SwitchTransition>
+				</TransitionGroup>
 			</div>
 		</Fragment>
 	)
 }
 
 export default App;
-
-
-
-
-// testing react hooks
-// const Todo = ({ todo }) => <div className="todo">{todo.text}</div>;
-
-// function TodoForm({ addTodo }) {
-//     const [value, setValue] = useState("");
-
-//     const handleSubmit = e => {
-//         e.preventDefault();
-//         if (!value) return;
-//         addTodo(value);
-//         setValue("");
-//     };
-
-//     return (
-//         <form onSubmit={handleSubmit}>
-//             <input
-//                 type="text"
-//                 className="input"
-//                 value={value}
-//                 onChange={e => setValue(e.target.value)}
-//             />
-//         </form>
-//     );
-// }
-
-// function TestTODO() {
-//     const [todos, setTodos] = useState(false);
-
-//     const addTodo = (text) => {
-//         setTodos(newTodos);
-//     };
-
-//     return (
-//         <div className="app">
-//             <div className="todo-list">{todos && "todo is true" || "todo is false"}
-//                 <TodoForm addTodo={addTodo} />
-//             </div>
-//         </div>
-//     );
-// }
