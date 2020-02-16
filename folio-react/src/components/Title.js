b4083709b593c041aa7a3cffa72bb9589d47d8d6
RolandWarburton <warburtonroland@gmail.com>
2020-02-16 21:52:34 +1100

    :fire: Giving up on slide transition effect

--- folio-react/src/components/Title.js
+++ folio-react/src/components/Title.js
@@ -1,6 +1,8 @@
 import React, { Component, useState, Children, useEffect, createContext, useContext, Fragment } from 'react';
 import { TransitionGroup, CSSTransition, Transition, SwitchTransition } from 'react-transition-group'
 import { Link } from 'react-router-dom';
+import { jsx, css, Global, ClassNames } from '@emotion/core'
+import styled from '@emotion/styled'
 import SVGIcon from './SVGIcon'
 
 // Container for the SVG links on the Left Hand Column 
@@ -15,34 +17,20 @@ const SVGContent = () => {
 }
 
 function Title({ location }) {
-	// Define a timeout for the page slide transition
-	let timeout = { enter: 700, exit: 700 };
-
-	// This is my attempt at the slide animation...
-	// create a hook for when the location changes
 	const [onPageRoot, setOnPageRoot] = useState(() => (location.pathname != "/") ? true : false)
-
-	let testStyle = (onPageRoot) ? {transform: "translate(-25vw)"} : {transform: "translateX(0)"}
 
 	// Flip between true and false to set the onPageRoot state.
 	useEffect(() => {
-		// When loaded or unloaded
 		setOnPageRoot(() => (location.pathname != "/") ? true : false)
-		return () => {
-			console.log("unloaded")
-		}
 	}, [location.pathname]);
 
 	return (
-		<CSSTransition timeout={timeout} in={onPageRoot} classNames="transform" key={location.key}>
-			<header className="LeftCol" style={testStyle}>
-				<h3 className="tester">Roland Warburton</h3>
-				<div className="NormalAnchor" href="https://goo.gl/maps/SEBJEfeRSAbGtLmv9" id="location">Melbourne, VIC</div>
-				<SVGContent />
-				<Link className="DarkHyperLink" to='/' >click me (home)</Link>
-				{/* <button onClick={() => setState(!state)}>manually set state</button> */}
-			</header >
-		</CSSTransition >
+		<header className="LeftCol">
+			<h3 className="tester">Roland Warburton</h3>
+			<div className="NormalAnchor" href="https://goo.gl/maps/SEBJEfeRSAbGtLmv9" id="location">Melbourne, VIC</div>
+			<SVGContent />
+			<Link className="DarkHyperLink" to='/' >click me (home)</Link>
+		</header >
 	);
 }
 
