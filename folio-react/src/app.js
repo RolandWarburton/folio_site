02be523e84a03f75489712a1ee1e41970b0265bd
RolandWarburton <warburtonroland@gmail.com>
2020-02-17 22:12:36 +1100

    :bug: Fixing Transitions again

--- folio-react/src/app.js
+++ folio-react/src/app.js
@@ -1,6 +1,6 @@
 import React, { Component, useState, useContext, useTransition, useEffect, Fragment, Children } from 'react';
 import { TransitionGroup, CSSTransition, Transition, SwitchTransition } from 'react-transition-group'
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route, Link, useParams } from 'react-router-dom';
 import '../styles/styles.scss'
 
 import Title from './components/Title';
@@ -17,22 +17,22 @@ const routes = [
 
 function App({ location }) {
 	// Set the timeout for the CSSTransition
-	const timeout = { enter: 100, exit: 100 }
+	const timeout = { enter: 200, exit: 200 }
 
 	return (
-		<SwitchTransition mode="out-in">
+		// <SwitchTransition mode="out-in">
 			<Fragment>
 			{routes.map(({ path, Component }) => (
 				<Route key={path} exact path={path}>
 					{({ match }) => (
-						<CSSTransition in={match != null} timeout={timeout} classNames={(location.pathname == "/") ? "fadeY" : "fadeX"} unmountOnExit>
+						<CSSTransition in={match != null} timeout={timeout} classNames={(path == "/") ? "fadeY" : "fadeX"} unmountOnExit>
 							<Component location={location}/>
 						</CSSTransition>
 					)}
 				</Route>
 			))}
 		</Fragment>
-			</SwitchTransition>
+			
 	)
 }
 
