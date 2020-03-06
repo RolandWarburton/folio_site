76b6fa286f1595eba74f22c6f271c51d17ff55c0
RolandWarburton <warburtonroland@gmail.com>
2020-03-06 21:20:36 +1100

    :sparkles: Add about page content

--- static-site-generator/src/views/about.js
+++ static-site-generator/src/views/about.js
@@ -1,8 +1,18 @@
 const about = `
 <div class='imageAndTextWrapper'>
     <aside>
-        Hi. I am a Computer enthusiast.\n
-        My interests are writing software, offensive and defensive cybersecurity, 
+        <p>
+            I am a Computer enthusiast writing software and building things.
+        </p>
+        <br />
+        <p>
+            My interests are hacking, automation, and talking about software design philosophy. 
+            My career goal is to work in devops / system administration by day and hack computers by night.
+        </p>
+        <br />
+        <p>
+            I am studying a BA of Computer Science in cybersecurity and network design at university.
+        </p>
     </aside>
     <svg class='avatar'/>
 </div>
