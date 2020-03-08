44b3403541d99249bfc619ff8ac5d36629765626
RolandWarburton <warburtonroland@gmail.com>
2020-03-08 23:00:19 +1100

    :bug: Fix ListFileInDir returning files in subdirs

--- static-site-generator/src/views/partials/listFilesInDir.js
+++ static-site-generator/src/views/partials/listFilesInDir.js
@@ -1,5 +1,17 @@
 const fs = require('fs')
 
+// takes a filepath string or array
+// returns the length of a route
+const lengthOfRoute = function (filepath) {
+    // check if the given filepath is an array
+    const isArray = Array.isArray(filepath)
+
+    // if its an array return its length
+    if (isArray) return filepath.length
+    // if its not an array convert it to one and return its length
+    else return filepath.split('/').length
+}
+
 // Returns an entire list of notes in the /src/views/notes dir
 // takes a filepath and will return all files from that point onwards
 // EG: 'notes/' will return: 'notes/notes1', 'notes/notes2'
@@ -11,7 +23,7 @@ module.exports = (path) => {
     // filter out routes to ones that are only on the targetPath (IE. inside the directory) 
     let output = initData.map(route =>
         `
-        ${route.filepath.includes(path) ?
+        ${(route.filepath.includes(path) && lengthOfRoute(route.filepath) == lengthOfRoute(path)) ?
         `
             <ul>
                 <li>
