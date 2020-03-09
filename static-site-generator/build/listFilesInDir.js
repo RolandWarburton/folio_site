b8fcdcec503a95353487523a04809148c93e2414
RolandWarburton <warburtonroland@gmail.com>
2020-03-09 21:27:17 +1100

    :ambulance: Fixed listFilesInDir function

--- static-site-generator/build/listFilesInDir.js
+++ static-site-generator/build/listFilesInDir.js
@@ -1,15 +1,16 @@
-const fs = require('fs')
-const path = require('path')
-const filepathHelper = require('./filepathHelper')
-const readFiles = require('./readFiles')
+const fp = require('./filepathHelper')
+const getPrevRoute = require('./getPrevRoute')
+
 // takes returns all page titles and filepaths in the given directory
-module.exports = (filepath) => {
+module.exports = (targetMap, filepath) => {
+    filepath = fp.sanitizeHTMLfilepath(filepath)
     const result = []
-    readFiles(path.resolve(process.cwd(), 'src/views', filepath), ({ filepath, title }) => {
-        result.push({
-            title: title,
-            filepath: filepath
-        })
-    }, false)
+    targetMap.forEach((route) => {
+        if (fp.lengthOfRoute(route.filepath) > fp.lengthOfRoute(filepath)) {
+            if (getPrevRoute(targetMap, route.filepath) == filepath) {
+                result.push(route)
+            }
+        }
+    })
     return result
 }
--- static-site-generator/tests/ListFilesInDir.spec.js
+++ static-site-generator/tests/ListFilesInDir.spec.js
@@ -1,26 +1,57 @@
 const fs = require('fs')
 const ListFilesInDir = require('../build/listFilesInDir')
+const generateTemplateMap = require('../build/generateTemplateMap')
 
 const notesSlashTools = [
     {
         "filepath": "notes/tools/toolsIntro",
+        "templatePath": "./templates/template-list-item.ejs",
         "title": "toolsIntro"
     },
 ]
 
 // const notes = ['bookmarks', 'bucketlist', 'ComputerScience', 'tools']
 const notes = [
-    { "filepath": "notes/ComputerScience", "title": "ComputerScience" }, 
-    { "filepath": "notes/bookmarks", "title": "bookmarks" }, 
-    { "filepath": "notes/bucketlist", "title": "bucketlist" }, 
-    { "filepath": "notes/tools", "title": "tools" }
+    {
+        "filepath": "notes/ComputerScience",
+        "templatePath": "./templates/template-list-item.ejs",
+        "title": "ComputerScience",
+    },
+    {
+        "filepath": "notes/bookmarks",
+        "templatePath": "./templates/template-list-item.ejs",
+        "title": "bookmarks",
+    },
+    {
+        "filepath": "notes/bucketlist",
+        "templatePath": "./templates/template-list-item.ejs",
+        "title": "bucketlist",
+    },
+    {
+        "filepath": "notes/computerScience/CSintro",
+        "templatePath": "./templates/template-list-item.ejs",
+        "title": "CSintro",
+    },
+    {
+        "filepath": "notes/linux/linuxIntro",
+        "templatePath": "./templates/template-list-item.ejs",
+        "title": "linuxIntro",
+    },
+    {
+        "filepath": "notes/tools",
+        "templatePath": "./templates/template-list-item.ejs",
+        "title": "tools",
+    },
 ]
+
 
 describe("Test ListAllFilesInDir", () => {
     test("return an object of routes in a given directory", () => {
+        let tpmap = generateTemplateMap()
+        // console.log(tpmap)
 
         const targetMap = JSON.parse(fs.readFileSync('./temp/templateMap.json'))
-        expect(ListFilesInDir('notes/tools/')).toEqual(notesSlashTools);
-        expect(ListFilesInDir('notes/')).toEqual(notes);
+        expect(ListFilesInDir(tpmap, 'notes/tools/')).toEqual(notesSlashTools);
+        expect(ListFilesInDir(tpmap, 'notes/')).toEqual(notes);
     });
 });
