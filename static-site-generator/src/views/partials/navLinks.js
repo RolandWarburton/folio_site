2bcb4720d13e25844249de17da891f308d73d43a
RolandWarburton <warburtonroland@gmail.com>
2020-03-09 16:07:49 +1100

    :bug: Fix navLinks rendering container when empty

--- static-site-generator/src/views/partials/navLinks.js
+++ static-site-generator/src/views/partials/navLinks.js
@@ -2,11 +2,25 @@ module.exports = (previous, next) => {
     const prevLink = (previous) ? `<a class="darkHyperLink" href=/${previous.filepath}>${previous.title}</a>` : '';
     const nextLink = (next) ? `<a class="darkHyperLink" href=/${next.filepath}>${next.title}</a>` : '';
     return (
-        `<nav id="dynamic-nav">
-            <li id="nav-prev-link">${prevLink}</li>
-            <li id="nav-next-link">${nextLink}</li>
-        </nav>`
+        `
+        ${(prevLink || nextLink) ? 
+        `
+            <nav id="dynamic-nav">
+                <li id="nav-prev-link">${prevLink}</li>
+                <li id="nav-next-link">${nextLink}</li>
+            </nav>
+        ` : ''
+        }
+        `
+        
     )
 }
 // i might use this home link one day for something useful
 {/* <li id="nav-home-link"><a class="darkHyperLink" href="/index.html">Home</a></li> */}
+
+// `
+// <nav id="dynamic-nav">
+//     <li id="nav-prev-link">${prevLink}</li>
+//     <li id="nav-next-link">${nextLink}</li>
+// </nav>
+// `
