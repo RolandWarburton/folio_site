4286e0916682a496231b7051f9afc47172917e3d
RolandWarburton <warburtonroland@gmail.com>
2020-03-10 22:18:45 +1100

    :bug: Got the dynamic nav bar working again :D

--- static-site-generator/build/processFiles.js
+++ static-site-generator/build/processFiles.js
@@ -4,6 +4,9 @@ const generatePage = require('./generatePage')
 const readFiles = require('./readFiles')
 const getTemplate = require('./getTemplate')
 const generateTemplateMap = require('./generateTemplateMap')
+const getRoutePositionInDir = require('./getRoutePositionInDir')
+const listFilesInDir = require('./listFilesInDir')
+const getPrevRoute = require('./getPrevRoute')
 
 // path where all the views are stored
 const viewsDir = path.resolve(process.cwd(), 'src/views')
@@ -19,21 +22,22 @@ readFiles(viewsDir, ({ title, filepath }) => {
 
 // get a list of templates that each file wants to use
 const templateMap = generateTemplateMap(routes)
-
-const getNav = (route, filepath) => {
-	const doesExist = (route) => {
-		return route != undefined
-	}
-
-	const isInSameFileDir = (route, filepath) => {
-		return path.parse(route.filepath).dir == path.parse(filepath).dir
-	}
-	
-	return (doesExist(route) && isInSameFileDir(route, filepath)) ? route : ''
-}
+console.log(`Read in ${templateMap.length} files`)
 
 routes.forEach((route, i) => {
 	const template = getTemplate(templateMap, route.title)
+
+	// get a next and previous route for the filepath in the current directory
+	// if no route is found or the relative index +-1 becomes undefined it will not be rendered
+	// relativeIndex gets the position the the current routes filepath. eg 0, 1 etc within its directory
+	const relativeIndex = getRoutePositionInDir(templateMap, route.filepath)
+	// get all the files in the parent of this filepath
+	const filesInDir = listFilesInDir(templateMap, getPrevRoute(templateMap, route.filepath))
+	// set next/prev filepath to one of the files in the parent directory of this filepath
+	// +-1 to get the offset neighbour from its relative index in the parent directory 
+	const next = filesInDir[relativeIndex + 1]
+	const prev = filesInDir[relativeIndex - 1]
+
 	pages.push(generatePage({
 		// Path needs to take a path without an extension. Examples: about, notes/blog
 		//where to write the file in dist
@@ -45,8 +49,8 @@ routes.forEach((route, i) => {
 		// tells EJS to use this js file to populate its template body 
 		target: route.filepath,
 		// previous and next in same route dir (if there is one)
-		previous: getNav(routes[i - 1], route.filepath),
-		next: getNav(routes[i + 1], route.filepath),
+		previous: prev,
+		next: next,
 		// favicon requires config.output.publicPath = '/' in webpack to work
 		favicon: './src/media/favicon.ico'
 	}))
--- static-site-generator/tests/getRoutePositionInDir.spec.js
+++ static-site-generator/tests/getRoutePositionInDir.spec.js
@@ -2,25 +2,16 @@ const fs = require('fs')
 const ListFilesInDir = require('../build/listFilesInDir')
 const generateTemplateMap = require('../build/generateTemplateMap')
 const listFilesInDir = require('../build/listFilesInDir')
+const getRoutePositionInDir = require('../build/getRoutePositionInDir')
 
 // listFilesInDir() will return a list of all the files in a directory
 // then we test to see if a particular filepath is contained in that array, and return its index
 
 describe("Test ListAllFilesInDir", () => {
-    test("return an object of routes in a given directory", () => {
+    test("returns the index of an a filepath in its own directory", () => {
         let templateMap = generateTemplateMap()
 
-        // return the position of notes/tools/toolsIntro in the notes/tools directory
-        const testFirstPos = listFilesInDir(templateMap, 'notes/tools').findIndex(function (route, i) {
-            return route.filepath === 'notes/tools/toolsIntro'
-        });
-
-        // return the position of notes/bookmarks in the notes/ directory
-        const testSecondPos = listFilesInDir(templateMap, 'notes/').findIndex(function (route, i) {
-            return route.filepath === 'notes/bookmarks'
-        });
-
-        expect(testFirstPos).toEqual(0);
-        expect(testSecondPos).toEqual(1);
+        expect(getRoutePositionInDir(templateMap, 'notes/tools')).toEqual(3);
+        expect(getRoutePositionInDir(templateMap, 'notes/tools/toolsIntro')).toEqual(0);
     });
 });
